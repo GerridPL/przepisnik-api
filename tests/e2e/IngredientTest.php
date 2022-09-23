d40048af51ab538bcd11a5a6cb330b701087eb33
Hubert Wanga <hubert.wanga@gmail.com>
2022-09-23 19:18:32 +0200

    Prepare e3e tests for ingredients.

--- tests/e2e/IngredientTest.php
+++ tests/e2e/IngredientTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\e2e;
 
+use App\Controller\IngredientController;
 use App\DataProvider\IngredientDataProvider;
 use App\DTO\IngredientDTO;
 use App\Entity\IngredientEntity;
@@ -13,82 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IngredientTest extends DatabaseDependantTestCase
 {
-    /** @test */
-    public function a_ingredient_service_return_ingredients(): void
-    {
-        // Setup
-        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
-        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
-        $ingredientService = new IngredientService($ingredientDataProvider);
-
-        $ingredientEntity = new IngredientEntity('bread');
-        $ingredientEntity2 = new IngredientEntity('butter');
-        $ingredientEntity3 = new IngredientEntity('water');
-        $this->entityManager->persist($ingredientEntity);
-        $this->entityManager->persist($ingredientEntity2);
-        $this->entityManager->persist($ingredientEntity3);
-        $this->entityManager->flush();
-
-        // Do something
-        $ingredientModels = $ingredientService->index();
-
-        // Make assertions
-        $this->assertEquals('bread', $ingredientModels[0]->getName());
-        $this->assertEquals('1', $ingredientModels[0]->getId());
-        $this->assertNotNull($ingredientModels[0]->getCreatedAt());
-        $this->assertNotNull($ingredientModels[0]->getUpdatedAt());
-        $this->assertEquals('butter', $ingredientModels[1]->getName());
-        $this->assertEquals('2', $ingredientModels[1]->getId());
-        $this->assertNotNull($ingredientModels[1]->getCreatedAt());
-        $this->assertNotNull($ingredientModels[1]->getUpdatedAt());
-        $this->assertEquals('water', $ingredientModels[2]->getName());
-        $this->assertEquals('3', $ingredientModels[2]->getId());
-        $this->assertNotNull($ingredientModels[2]->getCreatedAt());
-        $this->assertNotNull($ingredientModels[2]->getUpdatedAt());
-    }
-
-    /** @test */
-    public function a_ingredient_service_can_store_by_dto(): void
-    {
-        // Setup
-        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
-        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
-        $ingredientService = new IngredientService($ingredientDataProvider);
-        $ingredientDTO = IngredientDTO::fromRequest(new Request([], ['name' => 'pepper']));
-
-        // Do something
-        $ingredientService->create($ingredientDTO);
-        $ingredientModels = $ingredientService->index();
-
-        // Make assertions
-        $this->assertEquals('pepper', $ingredientModels[0]->getName());
-        $this->assertEquals('1', $ingredientModels[0]->getId());
-        $this->assertNotNull($ingredientModels[0]->getCreatedAt());
-        $this->assertNotNull($ingredientModels[0]->getUpdatedAt());
-    }
-
-    /** @test */
-    public function a_ingredient_service_can_update_by_dto_and_index(): void
-    {
-        // Setup
-        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
-        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
-        $ingredientService = new IngredientService($ingredientDataProvider);
-        $ingredientDTO = IngredientDTO::fromRequest(new Request([], ['name' => 'pepper']));
-        $ingredientDTOForUpdate = IngredientDTO::fromRequest(new Request([], ['name' => 'salt']));
-
-        // Do something
-        $ingredientService->create($ingredientDTO);
-        $ingredientService->update($ingredientDTOForUpdate, 1);
-        $ingredientModels = $ingredientService->index();
-
-        // Make assertions
-        $this->assertEquals('salt', $ingredientModels[0]->getName());
-        $this->assertEquals('1', $ingredientModels[0]->getId());
-        $this->assertNotNull($ingredientModels[0]->getCreatedAt());
-        $this->assertNotNull($ingredientModels[0]->getUpdatedAt());
-    }
-
     /** @test */
     public function a_ingredient_service_can_remove_by_id(): void
     {
@@ -105,5 +30,99 @@ class IngredientTest extends DatabaseDependantTestCase
 
         // Make assertions
         $this->assertEquals([], $ingredientModels);
+    }
+
+    /** @test */
+    public function a_ingredient_controller_can_create(): void
+    {
+        // Setup
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
+        $ingredientService = new IngredientService($ingredientDataProvider);
+        $ingredientController = new IngredientController();
+
+        $request = new Request([], ['name' => 'pepper']);
+
+        // Do something
+        $response = $ingredientController->create($request, $ingredientService);
+
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $storedIngredient = $ingredientRepository->findOneById(1);
+
+        // Make assertions
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals('pepper', $storedIngredient->getName());
+    }
+
+    /** @test */
+    public function a_ingredient_controller_can_show(): void
+    {
+        // Setup
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
+        $ingredientService = new IngredientService($ingredientDataProvider);
+        $ingredientController = new IngredientController();
+
+        $request1 = new Request([], ['name' => 'bread']);
+        $request2 = new Request([], ['name' => 'butter']);
+        $request3 = new Request([], ['name' => 'water']);
+
+        $ingredientController->create($request1, $ingredientService);
+        $ingredientController->create($request2, $ingredientService);
+        $ingredientController->create($request3, $ingredientService);
+
+        // Do something
+        $jsonResponse = $ingredientController->index($ingredientService);
+
+        // Make assertions
+        $this->assertEquals(200, $jsonResponse->getStatusCode());
+
+        $expectedJson = '[{"name":"bread","id":1},{"name":"butter","id":2},{"name":"water","id":3}]';
+        $this->assertJsonStringEqualsJsonString($expectedJson, $jsonResponse->getContent());
+    }
+
+    /** @test */
+    public function a_ingredient_controller_can_update(): void
+    {
+        // Setup
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
+        $ingredientService = new IngredientService($ingredientDataProvider);
+        $ingredientController = new IngredientController();
+
+        $requestForCreate = new Request([], ['name' => 'pepper']);
+        $ingredientController->create($requestForCreate, $ingredientService);
+
+        // Do something
+        $requestForUpdate = new Request([], ['name' => 'tomato']);
+        $responseFromUpdate = $ingredientController->update($requestForUpdate, $ingredientService, 1);
+
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $storedIngredient = $ingredientRepository->findOneById(1);
+
+        // Make assertions
+        $this->assertEquals(204, $responseFromUpdate->getStatusCode());
+        $this->assertEquals('tomato', $storedIngredient->getName());
+    }
+
+    /** @test */
+    public function a_ingredient_controller_can_delete(): void
+    {
+        // Setup
+        $ingredientRepository = $this->entityManager->getRepository(IngredientEntity::class);
+        $ingredientDataProvider = new IngredientDataProvider($ingredientRepository);
+        $ingredientService = new IngredientService($ingredientDataProvider);
+        $ingredientController = new IngredientController();
+
+        $requestForCreate = new Request([], ['name' => 'pepper']);
+        $ingredientController->create($requestForCreate, $ingredientService);
+
+        // Do something
+        $responseFromDelete = $ingredientController->delete($ingredientService, 1);
+        $responseFromIndex = $ingredientController->index($ingredientService);
+
+        // Make assertions
+        $this->assertEquals(204, $responseFromDelete->getStatusCode());
+        $this->assertJsonStringEqualsJsonString('[]', $responseFromIndex->getContent());
     }
 }
